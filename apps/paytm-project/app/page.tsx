2d7cea42eecbfd4ca6d156613bbaf4a7d9ed1e21
knilesh9 <neutron_stays@protonmail.com>
2024-06-16 13:54:49 +0530

    Init with @repo/db with pnmp init in ./packages and change package name

---
+++ apps/paytm-project/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hi There how are you?</h1>
+    </div>
+  );
+}
